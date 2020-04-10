d3f620a4f29a508e7bdfc22bb59867a4424195ff
kevllino <kevin.eid3@mail.dcu.ie>
2020-04-10 20:57:38 +0100

    Add configuration

--- firehose_publisher.py
+++ firehose_publisher.py
@@ -1,10 +1,17 @@
 from random import randint
 import boto3
 from pytictoc import TicToc
+import hcl
 
 timer = TicToc()
 car_manufacturers = ["Tesla", "Toyota", "Renault"]
 ev_providers = ["Octopus", "EDF"]
+
+
+def read_config(filename):
+    with open(filename, 'r') as fp:
+        object = hcl.load(fp)
+    return object
 
 
 def generate_random_signals(signal_size):
@@ -12,8 +19,7 @@ def generate_random_signals(signal_size):
     user_signals = []
 
     for i in range(signal_size):
-        car_signals.append(
-            {"cableId": i, "carManufacturer": car_manufacturers[randint(0, len(car_manufacturers) - 1)]})
+        car_signals.append({"cableId": i, "carManufacturer": car_manufacturers[randint(0, len(car_manufacturers) - 1)]})
         user_signals.append({"userId": i, "evProvider": ev_providers[randint(0, len(ev_providers) - 1)]})
 
     return car_signals, user_signals
@@ -38,7 +44,10 @@ def put_record_batch(client, delivery_stream, records):
 
 
 if __name__ == '__main__':
-    client = boto3.client('firehose', region_name="eu-west-2")
+    object = read_config("terraform/terraform.tfvars")
+    car_stream_name, user_stream_name = object["car_stream_name"], object["user_stream_name"]
+
+    client = boto3.client('firehose', region_name=object["region"])
 
     car_signals, user_signals = generate_random_signals(500)
     random_signals = car_signals + user_signals
@@ -46,12 +55,12 @@ if __name__ == '__main__':
     timer.tic()
     for signal in random_signals:
         if "cableId" in signal.keys():
-            put_record(client, delivery_stream='cable-firehose-stream', record=signal)
+            put_record(client, delivery_stream=car_stream_name, record=signal)
         elif "userId" in signal.keys():
-            put_record(client, delivery_stream='user-firehose-stream', record=signal)
+            put_record(client, delivery_stream=user_stream_name, record=signal)
     timer.toc("Put Record")
 
     timer.tic()
-    put_record_batch(client, delivery_stream='cable-firehose-stream', records=car_signals)
-    put_record_batch(client, delivery_stream='user-firehose-stream', records=user_signals)
+    put_record_batch(client, delivery_stream=car_stream_name, records=car_signals)
+    put_record_batch(client, delivery_stream=user_stream_name, records=user_signals)
     timer.toc("Put Records")
