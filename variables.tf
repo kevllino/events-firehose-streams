c19f44dfbff0a40f952272869a019146ca814ad1
kevllino <kevin.eid3@mail.dcu.ie>
2020-04-23 08:55:26 +0100

    Add default values to variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "region" {
   type = string
+  default = "eu-west-2"
 }
 
 variable "car_stream_name" {
   type = string
+  default = "car-firehose-stream"
 }
 
 variable "user_stream_name" {
   type = string
+  default = "user-firehose-stream"
 }
